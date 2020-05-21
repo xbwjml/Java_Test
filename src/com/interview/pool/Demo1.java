892df3bbd8c1b68a2e31649150313fcd4d258f20
LMJ <Leeminjie1995@126.com>
2020-05-21 21:03:57 +0800

    线程池常用的三种创建方法

---
+++ src/com/interview/pool/Demo1.java
@@ -0,0 +1,25 @@
+package com.interview.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Demo1 {
+	public static void main(String[] args) {
+//		ExecutorService threadPool = Executors.newFixedThreadPool(5);//一池5个处理线程
+//		ExecutorService threadPool = Executors.newSingleThreadExecutor();//一池1个处理线程
+		ExecutorService threadPool = Executors.newCachedThreadPool();//一池不确定个处理线程
+		
+		try {
+			//模拟10个用户，每个用户就是来自外部的请求
+			for( int i=1; i<=10; i++ ){
+				threadPool.execute( ()->{
+					System.out.println(Thread.currentThread().getName()+"\t 办理业务");
+				});
+			}
+		} catch( Exception e ) {
+			e.printStackTrace();
+		} finally {
+			threadPool.shutdown();
+		}
+	}
+}
