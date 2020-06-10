0fdfa5cf0d01b21e92cee9537326fb79f6a90a36
LMJ <Leeminjie1995@126.com>
2020-06-10 16:59:37 +0800

    饿汉式 (静态代码块)

---
+++ src/com/designPattern/singleton/Test02.java
@@ -0,0 +1,31 @@
+package com.designPattern.singleton;
+
+public class Test02 {
+	public static void main(String[] args) {
+		Singleton01 instance = Singleton01.getInstance();
+		Singleton01 instance2 = Singleton01.getInstance();
+		System.out.println(instance == instance2);
+	}
+}
+
+/**
+ * 饿汉式(静态代码块)
+ * @author Leemi
+ */
+class Singleton02{
+	//1.构造方法私有化
+	private Singleton02() {}
+	
+	private static Singleton02 instance;
+	
+	//2.在静态代码块中创建对象
+	static {
+		Singleton02 instance = new Singleton02();
+	}
+	
+	//3.对外提供一个公有的静态方法，返回实例对象
+	public static Singleton02 getInstance() {
+		return instance;
+	}
+	
+}
